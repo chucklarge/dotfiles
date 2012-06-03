1d4663f90389db1e507096e2dbf2348523fefeb0
Charles Clark <chucklarge@gmail.com>
2012-06-03 17:50:30 -0400

    beter symlink script

--- symlinks.sh
+++ symlinks.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-ln -s ~/.dotfiles/bash_aliases ~/.bash_aliases
-ln -s ~/.dotfiles/ackrc ~/.ackrc
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/inputrc ~/.inputrc
-ln -s ~/.dotfiles/editrc ~/.editrc
-ln -s ~/.dotfiles/colordiffrc ~/.colordiffrc
+
+for i in *; do
+    if [ "$i" != "README.md" ] && [ "$i" != "symlinks.sh" ]; then
+        l="$HOME/.$i"
+        if [ ! -h $l ]; then
+            ln -s $(pwd)/$i $l
+            echo "created $l"
+        fi
+    fi
+done
